d89ac1f44ff27bc2f5a507517496e1ece803bf78
Diellza Raçi <115530290+didiellzaza@users.noreply.github.com>
2023-11-30 17:48:33 +0100

    Update server.cs

--- server.cs
+++ server.cs
@@ -6,7 +6,36 @@ using System.IO;
 
 public class MultiThreadedEchoServer
 {
-    
+    private static void ProcessClientRequests(object argument)
+    {
+        TcpClient client = (TcpClient)argument;
+        try
+        {
+            StreamReader reader = new(client.GetStream());
+            StreamWriter writer = new(client.GetStream());
+            string s = String.Empty;
+            while (!(s = reader.ReadLine()).Equals("Exit", StringComparison.Ordinal) || s == null)
+            {
+                Console.WriteLine("Nga Klienti =>> " + s);
+                writer.WriteLine("Nga Serveri =>> " + s);
+                writer.Flush();
+            }
+            reader.Close();
+            writer.Close();
+            client.Close();
+            Console.WriteLine("Duke mbyllur lidhjen e klientit...");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Problem komunikimi me klientin. Exiting thread.");
+        }
+        finally
+        {
+            client?.Close();
+        }
+    }
+
+
 
 }
 
