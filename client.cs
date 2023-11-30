a6a20fcd68a4f71c1d5e3a9de3d896d7af15f321
dinapirana <122113432+dinapirana@users.noreply.github.com>
2023-11-30 18:09:17 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -12,7 +12,30 @@ namespace Rrjeta
             {
                 throw new ArgumentNullException(nameof(reader));
             }
-
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 8080);
+                reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                string s = string.Empty;
+                while (!s.Equals("Exit", StringComparison.Ordinal))
+                {
+                    Console.Write("Futni një string për ta dërguar te serveri: ");
+                    s = Console.ReadLine();
+                    Console.WriteLine();
+                    writer.WriteLine(s);
+                    writer.Flush();
+                    string server_string = reader.ReadLine();
+                    Console.WriteLine(server_string);
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
            
         }
     }
