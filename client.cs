b7ffd458c1b80d8a2caf64f6513d8052021ac73b
dinapirana <122113432+dinapirana@users.noreply.github.com>
2023-11-30 17:57:04 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,1 +1,20 @@
+using System.IO;
+using System.Net.Sockets;
+using System;
+
+namespace Rrjeta
+{
+    public class EchoClient
+    {
+        public void StartClient(StreamReader reader)
+        {
+            if (reader is null)
+            {
+                throw new ArgumentNullException(nameof(reader));
+            }
+
+           
+        }
+    }
+}
 
