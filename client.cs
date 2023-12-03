a9be51c49a1015fffe52d40748e8f420b2b9873c
Diellza Raçi <115530290+didiellzaza@users.noreply.github.com>
2023-12-03 17:44:10 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -12,7 +12,39 @@ class UDPClient
         UdpClient client = new UdpClient();
         IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(Host), Port);
 
-        ///
+        Console.Write("Enter credentials (username:password): ");
+        string credentials = Console.ReadLine();
+
+        if (credentials != null)
+        {
+            byte[] credentialsBytes = Encoding.ASCII.GetBytes(credentials);
+            client.Send(credentialsBytes, credentialsBytes.Length, serverEndpoint);
+
+            byte[] authResponseBytes = client.Receive(ref serverEndpoint);
+            string authResponse = Encoding.ASCII.GetString(authResponseBytes);
+            Console.WriteLine("Server response: " + authResponse);
+
+            if (authResponse == "Authentication successful. Access granted.")
+            {
+                bool isReadOnly = credentials == "readonly:readonly123";
+
+                while (true)
+                {
+                    if (isReadOnly)
+                    {
+                        Console.Write("You are in read-only mode. Enter 'read <file_name>' or type 'exit' to end: ");
+                    }
+                    else
+                    {
+                        Console.Write("Enter a command ('read <file_name>', 'write <string> <file_name>', 'delete <file_name>' or type 'exit' to end): ");
+                    }
+
+                    string command = Console.ReadLine();
+
+                    ///
+                }
+            }
+        }
         client.Close();
     }
 }
