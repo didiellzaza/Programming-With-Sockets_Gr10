1fdc5c1bf877a1a728a446f08d47b9798c48f0a4
Diellza Raçi <115530290+didiellzaza@users.noreply.github.com>
2023-12-03 17:34:48 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -6,56 +5,14 @@ using System.Text;
 class UDPClient
 {
     private const int Port = 6000;
-    private static string Host = "192.168.0.20"; // IP adresa e serverit
+    private static string Host = "172.20.10.2";
 
     static void Main(string[] args)
     {
         UdpClient client = new UdpClient();
         IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(Host), Port);
 
-        Console.Write("Enter credentials (username:password): ");
-        string credentials = Console.ReadLine();
-
-        if (credentials != null)
-        {
-            byte[] credentialsBytes = Encoding.ASCII.GetBytes(credentials);
-            client.Send(credentialsBytes, credentialsBytes.Length, serverEndpoint);
-
-            byte[] authResponseBytes = client.Receive(ref serverEndpoint);
-            string authResponse = Encoding.ASCII.GetString(authResponseBytes);
-            Console.WriteLine("Server response: " + authResponse);
-
-            if (authResponse == "Authentication successful. Access granted.")
-            {
-                while (true)
-                {
-                    Console.Write("Enter a command ('read <file_name>', 'write <string> <file_name>', 'delete <file_name>' or type 'exit' to end): ");
-                    string command = Console.ReadLine();
-
-                    if (command == "exit")
-                    {
-                        const string exitMessage = "Client disconnected.";
-                        byte[] exitMessageBytes = Encoding.ASCII.GetBytes(exitMessage);
-                        client.Send(exitMessageBytes, exitMessageBytes.Length, serverEndpoint);
-                        break;
-                    }
-
-                    byte[] commandBytes = Encoding.ASCII.GetBytes(command);
-                    client.Send(commandBytes, commandBytes.Length, serverEndpoint);
-
-                    byte[] responseBytes = client.Receive(ref serverEndpoint);
-                    string response = Encoding.ASCII.GetString(responseBytes);
-                    Console.WriteLine("Server Response: " + response);
-
-                    if (response == "Limit of clients reached!")
-                    {
-                        Console.WriteLine("Disconnecting the client...");
-                        break;
-                    }
-                }
-            }
-        }
-
+        ///
         client.Close();
     }
 }
